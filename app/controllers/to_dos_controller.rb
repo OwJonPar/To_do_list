5b858be1ea7659fe116c85b5b7a8dfe97def978b
Owen <owenrham@gmail.com>
2015-02-27 11:57:30 -0500

    Create new page

--- app/controllers/to_dos_controller.rb
+++ app/controllers/to_dos_controller.rb
@@ -1,24 +1,49 @@
 class ToDosController < ApplicationController
+  before_action :set_to_do, only: [:show, :edit, :update, :destroy]
+
   def index
     @to_dos = ToDo.all
   end
 
   def new
+    @to_do = ToDo.new
   end
 
   def edit
   end
 
-  def create
-  end
-
   def show
   end
 
+  def create
+    @to_do = ToDo.new(to_do_params)
+    if @to_do.save
+      redirect_to root_path, notice: 'To Do was successfully created.'
+    else
+      render :new
+    end
+  end
+
   def update
+    if @to_do.update(to_do_params)
+      redirect_to root_path, notice: 'To Do was successfully updated.'
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @to_do.destroy
+    redirect_to to_dos_url, notice: 'To Do was successfully destroyed.'
   end
+
+  private
+    def set_to_do
+      @to_do = ToDo.find(params[:id])
+    end
+
+    def to_do_params
+      params.require(:to_do).permit(:text, :row_order)
+    end
 
 end
